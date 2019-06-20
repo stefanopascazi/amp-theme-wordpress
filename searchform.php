c559beea806b97b895e0c928a4185e6a2a372055
Stefano <stefanopascazi@gmail.com>
2019-06-20 17:28:41 +0200

    insert search form

---
+++ searchform.php
@@ -0,0 +1,6 @@
+<form role="search" method="GET" id="searchform" action="<?php echo home_url( '/' ); ?>" target="_top">
+    <div><label class="screen-reader-text" for="s">Cerca:</label>
+        <input type="text" value="" name="s" id="s" />
+        <input type="submit" id="searchsubmit" value="Cerca" />
+    </div>
+</form>
